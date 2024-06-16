0d98280d35b136e45d0d6c265c28dd1904dbab7f
sarojrmc <164706806+sarojrmc@users.noreply.github.com>
2024-06-16 22:10:01 +0545

    Update saroj.py

--- saroj.py
+++ saroj.py
@@ -1,4 +1,4 @@
-### ORIGINAL MAKER IS SAROJ DON ####
+#### ORIGINAL MAKER IS SAROJ DON ####
 
 #××××××××××IMPORT MODULES××××××××××#
 import os,zlib
@@ -12,4 +12,24 @@ try:
 except ImportError:
     print('\n  installing Requests ...\n')
     os.system('pip install requests')
-	
+
+try:
+    import concurrent.futures
+except ImportError:
+    print('\n  installing futures ...\n')
+    os.system('pip install futures')
+
+try:
+    import mechanize
+except ModuleNotFoundError:
+    os.system('pip install mechanize > /dev/null')
+
+from urllib.request import Request, urlopen
+import os, requests, re,platform, sys, random, subprocess, threading, itertools,base64,uuid,zlib,re,json,uuid,subprocess,shutil,webbrowser,time,json,sys,random,datetime,time,re,subprocess,platform,string,json,time,re,random,sys,string,uuid
+from concurrent.futures import ThreadPoolExecutor as SAROJgod
+from string import * 
+from random import randint
+from time import sleep as slp
+from os import system as cmd
+from zlib import decompress 
+import os, platform
